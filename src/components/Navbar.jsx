53bb9c21a9bf6e118fa6fc1b766b9987e70f1aee
rafasilvadev <rafabdasilva12@gmail.com>
2023-08-15 17:36:12 -0300

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "../routes/stylesheets/Navbar.css";
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBNavbar,
+  MDBNavbarToggler,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [showAnimated2, setShowAnimated2] = useState(false);
+
   return (
     <div className="navbar-body">
-      <div className="logo">
-        <h1 className="logo-text">MESTRE SALA</h1>
-      </div>
-      <div className="dropdown">⬜</div>
+      <MDBNavbar className="nav-menu">
+        <MDBContainer fluid>
+          <div className="logo">
+            <h1 className="logo-text">MESTRE SALA</h1>
+          </div>
+          <MDBNavbarToggler
+            type="button"
+            className="ms-auto"
+            data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            onClick={() => setShowAnimated2(!showAnimated2)}
+          >
+            <div className={`animated-icon2 ${showAnimated2 && "open"}`}>
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </MDBNavbarToggler>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <MDBCollapse show={showAnimated2}>
+        <div className="navbar-content">
+          <h3>Link 1</h3>
+          <h3>Link 2</h3>
+          <Link to="/login">Sair</Link>
+        </div>
+      </MDBCollapse>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
